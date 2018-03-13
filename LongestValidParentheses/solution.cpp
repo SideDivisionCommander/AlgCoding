066c486870faf2f8ff83bbc592b7d53f322890da
SideDivisionCommander <liuxiqin0612@outlook.com>
2018-03-13 18:31:36 +0800

    Upload Longest Valid Parent theses version1, using stack

--- LongestValidParentheses/solution.cpp
+++ LongestValidParentheses/solution.cpp
@@ -2,19 +2,31 @@ class Solution {
 public:
     int longestValidParentheses(string s) {
         int result = 0;
-        list<char> stack;
-        stack.clear();
-        list<char>::iterator it = stack.begin();
+        list<int> stack1, stack2;
+        stack1.clear();
+        stack2.clear();
         for(int i = 0; i < s.length(); ++i){
             if(s[i] == '('){
-                stack.push_back('(');
+                stack1.push_back(i);
                 continue;
             }
-            if(!stack.empty()){
-                stack.pop_back();
-                result += 2;
+            if(!stack1.empty()){
+                stack1.pop_back();
+                continue;
             }
+            stack2.push_back(i);
         }
-        return result;
+        stack1.merge(stack2);
+        stack1.sort();
+        int lastIndex = -1;
+        for(list<int>::iterator it = stack1.begin(); it != stack1.end(); ++it){
+            if (*it - lastIndex > result){
+                result = *it - lastIndex;
+            }
+            lastIndex = *it;
+        }
+        if(0 == stack1.size()) return s.length();
+        if(s.length() - stack1.back() > result) result = s.length() - stack1.back();
+        return result - 1;
     }
 };
