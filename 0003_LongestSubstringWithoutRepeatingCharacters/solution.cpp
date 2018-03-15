1f39bb9c567fa7ab3ebb24d7c3027c77294773a6
SideDivisionCommander <liuxiqin0612@outlook.com>
2018-03-15 12:00:25 +0800

    Upload 0003, version 1

---
+++ 0003_LongestSubstringWithoutRepeatingCharacters/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        string result, tmpResult;
+        result.clear();
+        tmpResult.clear();
+        for (int i = 0; i < s.length(); ++i){
+            size_t found = tmpResult.find(s[i]);
+            if(found != string::npos){
+                if(tmpResult.length() > result.length()){
+                    result = tmpResult;
+                }
+                tmpResult = tmpResult.erase(0, found+1);
+            }
+            tmpResult.append(1, s[i]);
+        }
+        if(tmpResult.length() > result.length()){
+            result = tmpResult;
+        }
+        return result.length();
+    }  
+};
